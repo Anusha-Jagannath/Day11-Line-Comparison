5902be2a8696606b23306c10ecffd71cbb84a3e4
Anusha-Jagannath <anushajaganath6@gmail.com>
2021-08-31 19:19:05 +0530

    length of line code added

--- src/com/linecomparison/LineComparison.java
+++ src/com/linecomparison/LineComparison.java
@@ -1,9 +1,24 @@
 package com.linecomparison;
 
+import java.util.Scanner;
+
 public class LineComparison {
 
 	public static void main(String[] args) {
+		int x1,y1,x2,y2;
+		float result;
 		System.out.println("Welcome to Line Comparison Problem Program");
+		Scanner s1 = new Scanner(System.in);
+		System.out.println("Enter value of x1");
+		x1 = s1.nextInt();
+		System.out.println("Enter value of y1");
+		y1 = s1.nextInt();
+		System.out.println("Enter value of x2");
+		x2 = s1.nextInt();
+		System.out.println("Enter value of y2");
+		y2 = s1.nextInt();
+		result = (float)Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+		System.out.println("Distance between two points is "+result);
 	}
 
 }
